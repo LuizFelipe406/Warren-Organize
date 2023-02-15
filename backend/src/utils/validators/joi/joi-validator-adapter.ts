8f8ba52a1cd2ac7c87d3cf558650d0231852d606
LuizFelipe406 <luizfpereira406@gmail.com>
2023-02-15 13:27:07 -0300

    refactor: Change Joi schema to proper folder

---
+++ backend/src/utils/validators/joi/joi-validator-adapter.ts
@@ -0,0 +1,16 @@
+import Joi from "joi"
+import { Validation } from "../validation";
+
+export class JoiAccountValidatorAdapter implements Validation {
+  private schema: Joi.ObjectSchema
+
+  constructor (schema: Joi.ObjectSchema) {
+    this.schema = schema
+  }
+
+  validate(input: unknown): Error | undefined {
+    const { error } = this.schema.validate(input)
+    if (error) return new Error(error.message)
+  }
+  
+}
---
+++ backend/src/utils/validators/joi/schemas/create-account-schema.ts
@@ -0,0 +1,9 @@
+import Joi from "joi";
+
+export const schema = Joi.object({
+  password: Joi.string().min(6).required(),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+    .required(),
+  name: Joi.string().min(3).required(),
+})
