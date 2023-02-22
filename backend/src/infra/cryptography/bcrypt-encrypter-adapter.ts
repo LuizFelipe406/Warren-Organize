a1ab3237fb37c5fef3b8a4b2652a815356fa30b8
LuizFelipe406 <luizfpereira406@gmail.com>
2023-02-22 17:18:13 -0300

    refactor: Folder Organization

--- backend/src/infra/cryptography/bcrypt-encrypter-adapter.ts
+++ backend/src/infra/cryptography/bcrypt-encrypter-adapter.ts
@@ -1,7 +1,7 @@
 import bcrypt from "bcryptjs"
 import Encrypter from "../../data/protocols/cryptography/encrypter";
 
-export class BCryptAdapter implements Encrypter {
+export class BCryptEncrypterAdapter implements Encrypter {
   encrypt(input: string): string {
     const SALT = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(input, SALT);
