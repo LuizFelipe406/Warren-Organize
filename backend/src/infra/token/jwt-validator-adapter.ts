0907f3bf9ca8fd0bcd996c86da0142d9aea20234
LuizFelipe406 <luizfpereira406@gmail.com>
2023-03-03 10:14:12 -0300

    feat: Add Token Validator

---
+++ backend/src/infra/token/jwt-validator-adapter.ts
@@ -0,0 +1,19 @@
+import jwt = require('jsonwebtoken');
+import { TokenValidator } from '../../presentation/protocols/token-validator';
+import { JwtPayload } from './jwt-generate-adapter';
+
+export default class JwtValidatorAdapter implements TokenValidator {
+  private secret = process.env.JWT_SECRET || 'jwt_secret';
+
+  validate(token: string | undefined): string | number {
+    if (!token) throw new Error("token not found")
+
+    try {
+      const payload = jwt.verify(token, this.secret) as JwtPayload;
+      return payload.id
+    } catch (error) {
+      throw new Error(error)
+    }
+  }
+  
+}
