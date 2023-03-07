12a1baa66d77029b2bbb5f199b34ff44fa7f04cb
LuizFelipe406 <luizfpereira406@gmail.com>
2023-03-07 11:42:54 -0300

    fix: Adjust Create Money Movement Schema

--- backend/src/utils/validators/joi/schemas/create-money-movement-schema.ts
+++ backend/src/utils/validators/joi/schemas/create-money-movement-schema.ts
@@ -1,7 +1,6 @@
 import Joi from "joi";
 
 export const schema = Joi.object({
-  userId: Joi.alternatives().try(Joi.number(), Joi.string()).required(),
   name: Joi.string().required(),
   amount: Joi.number().required(),
   date: Joi.date().required(),
