41f777e64535e174608c0a6f5f1034a68ea9e55d
LuizFelipe406 <luizfpereira406@gmail.com>
2023-03-07 11:43:31 -0300

    feat: Create Sequelize migrations for Expense and Revenue

---
+++ backend/src/infra/db/mysql/migrations/20230307143035-create-revenues.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-undef */
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Revenues', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      amount: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      category: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      userId: {
+        field: 'user_id',
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Accounts',
+          key: 'id'
+        }
+      }
+    });
+  },
+
+  async down (queryInterface) {
+    await queryInterface.dropTable('Revenues');
+  }
+};
